1cf35b468da99225fcfd69c8a1b6fde2d5465367
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2025-04-25 11:53:24 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    age = int(input("Ingrese un año:"))
+    if age % 100 == 0 and age % 400 != 0:
+        print("El año " + str(age) + " no es bisiesto")
+    elif age % 4 != 0:
+        print("El año " + str(age) + " no es bisiesto")
+    else:
+        print("El año " + str(age) + " es bisiesto")
